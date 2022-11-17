76d140e788cb5a9ee27ec0c13e0c218cc5408639
PVNagel <philip.v.nagel@gmail.com>
2022-11-17 16:57:47 +0100

    Where I got to 16:57

---
+++ Ex27-WPFAndMVVM2/WPFAndMVVM2-udleveret/WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel
+    {
+        private PersonRepository personRepo = new PersonRepository();
+        public List<PersonViewModel> PersonsVM { get; set; } = new();
+
+        public MainViewModel()
+        {
+            foreach (Person person in personRepo.GetAll())
+            {
+                PersonViewModel personVM = new(person);
+                PersonsVM.Add(personVM);
+            }
+        }
+    }
+}
