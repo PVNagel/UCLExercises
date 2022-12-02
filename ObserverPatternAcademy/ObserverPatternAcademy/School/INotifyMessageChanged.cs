5362a6514b6b3d36620f30fe204b165577142588
PVNagel <philip.v.nagel@gmail.com>
2022-12-02 14:04:44 +0100

    message

--- ObserverPatternAcademy/ObserverPatternAcademy/School/INotifyMessageChanged.cs
+++ ObserverPatternAcademy/ObserverPatternAcademy/School/INotifyMessageChanged.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ObserverPatternAcademy.Interfaces
+namespace ObserverPatternAcademy.School
 {
-    public interface IObserver
+    public interface INotifyMessageChanged
     {
-        void Update(object sender, EventArgs e);
+        public event EventHandler MessageChanged;
+
     }
 }
