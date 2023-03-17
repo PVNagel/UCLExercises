aeb30d2fd51ee7d4eb7c98a6fb76752be03bba0c
PVNagel <philip.v.nagel@gmail.com>
2023-03-17 10:22:58 +0100

    Added Age exercise to ExtensionMethods

---
+++ ExtensionMethods/ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Returns the difference between dates in years.
+        /// </summary>
+        /// <param name="birthDate"></param>
+        /// <param name="referenceDate"></param>
+        /// <returns>
+        /// Age from given birthdate compared to reference date.
+        /// </returns>
+        public static int Age(this DateTime birthDate, DateTime referenceDate)
+        {
+            double daysBetween = (referenceDate - birthDate).TotalDays;
+            if (daysBetween < 366)
+            {
+                return 0;
+            }
+            else if (daysBetween > 365)
+            {
+                return (int)daysBetween / 365;
+            }
+            else
+                return 0;
+        }
+    }
+}
--- ExtensionMethods/ExtensionMethodsTestProject/UnitTest1.cs
+++ ExtensionMethods/ExtensionMethodsTestProject/UnitTest1.cs
@@ -114,5 +114,117 @@ namespace ExtensionMethodsTestProject
             // Assert
             Assert.AreEqual("", shifted);
         }
+
+        [TestMethod]
+        public void Age_ReturnsAge_WhenReferenceDateIsSupplied()
+        {
+            // Arrange
+            DateTime birthDate = new DateTime(2003, 3, 1);
+            DateTime referenceDate = new DateTime(2023, 3, 5);
+
+            // Act
+            int age = birthDate.Age(referenceDate);
+
+            // Assert
+            Assert.AreEqual(20, age);
+        }
+
+        [TestMethod]
+        public void Age_ReturnsZero_WhenReferenceDateIsSameAsBirthDate()
+        {
+            // Arrange
+            DateTime birthDate = new DateTime(2000, 1, 1);
+            DateTime referenceDate = new DateTime(2000, 1, 1);
+
+            // Act
+            int age = birthDate.Age(referenceDate);
+
+            // Assert
+            Assert.AreEqual(0, age);
+        }
+
+        [TestMethod]
+        public void Age_ReturnsOne_WhenReferenceDateIsOneYearAfterBirthDate()
+        {
+            // Arrange
+            DateTime birthDate = new DateTime(2000, 1, 1);
+            DateTime referenceDate = new DateTime(2001, 1, 1);
+
+            // Act
+            int age = birthDate.Age(referenceDate);
+
+            // Assert
+            Assert.AreEqual(1, age);
+        }
+
+        [TestMethod]
+        public void Age_ReturnsZero_WhenReferenceDateIsBeforeBirthDate()
+        {
+            // Arrange
+            DateTime birthDate = new DateTime(2000, 1, 1);
+            DateTime referenceDate = new DateTime(1999, 1, 1);
+
+            // Act
+            int age = birthDate.Age(referenceDate);
+
+            // Assert
+            Assert.AreEqual(0, age);
+        }
+
+        [TestMethod]
+        public void Age_ReturnsZero_WhenReferenceDateIsOneDayBeforeBirthDate()
+        {
+            // Arrange
+            DateTime birthDate = new DateTime(2000, 1, 1);
+            DateTime referenceDate = new DateTime(1999, 12, 31);
+
+            // Act
+            int age = birthDate.Age(referenceDate);
+
+            // Assert
+            Assert.AreEqual(0, age);
+        }
+
+        [TestMethod]
+        public void Age_ReturnsZero_WhenReferenceDateIsOneDayBeforeLeapYear()
+        {
+            // Arrange
+            DateTime birthDate = new DateTime(2000, 2, 29);
+            DateTime referenceDate = new DateTime(2001, 2, 28);
+
+            // Act
+            int age = birthDate.Age(referenceDate);
+
+            // Assert
+            Assert.AreEqual(0, age);
+        }
+
+        [TestMethod]
+        public void Age_ReturnsTen_WhenReferenceDateIsTenYearsAfterBirthDate()
+        {
+            // Arrange
+            DateTime birthDate = new DateTime(2000, 1, 1);
+            DateTime referenceDate = new DateTime(2010, 1, 1);
+
+            // Act
+            int age = birthDate.Age(referenceDate);
+
+            // Assert
+            Assert.AreEqual(10, age);
+        }
+
+        // BONUS - need to use "DateTime?"
+        //[TestMethod]
+        //public void Age_ReturnsAge_WhenReferenceDateIsNotSupplied()
+        //{
+        //    // Arrange
+        //    DateTime birthDate = new DateTime(1990, 1, 1);
+
+        //    // Act
+        //    int age = birthDate.Age();
+
+        //    // Assert
+        //    Assert.AreEqual(DateTime.Now.Year - birthDate.Year, age);
+        //}
     }
 }
