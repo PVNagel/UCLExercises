3caf894204ed6eb37511f299b6520457c8d94ce0
PVNagel <philip.v.nagel@gmail.com>
2023-08-23 10:53:59 +0200

    added MvcMovies for Simon

---
+++ MvcMovies/MvcMovies/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovies.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID)
+        {
+            return HtmlEncoder.Default.Encode($"Welcome {name} you {ID} old fucker");
+        }
+    }
+}
